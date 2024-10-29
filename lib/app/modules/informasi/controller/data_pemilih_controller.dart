440b67bc103de09d6de7be193dff2f55b63b7804
saniap <saniakhzam@gmail.com>
2024-10-29 08:10:43 +0700

    feat: information page

--- lib/app/modules/informasi/controller/data_pemilih_controller.dart
+++ lib/app/modules/informasi/controller/data_pemilih_controller.dart
@@ -1,3 +1,14 @@
 import 'package:get/get.dart';
 
-class InformasiController extends GetxController {}
+class InformasiController extends GetxController {
+  final List<String> penting = [
+    "Menjamin terpilihnya pemimpin yang bertanggung jawab dan kompeten,",
+    "Mewujudkan kebijakan yang relevan dengan kebutuhan masyarakat, dan",
+    "Menguatkan demokrasi serta menjamin hak setiap warga negara.",
+  ];
+  final List<String> hak = [
+    "Anda adalah Warga Negara Indonesia.",
+    "Berusia 17 tahun atau lebih pada hari pemilihan, atau telah menikah.",
+    "Terdaftar dalam Daftar Pemilih Tetap (DPT).",
+  ];
+}
--- lib/app/modules/informasi/view/data_pemilih_view.dart
+++ lib/app/modules/informasi/view/data_pemilih_view.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:serkom/app/constant/font_constant.dart';
 
 import '../controller/data_pemilih_controller.dart';
 
@@ -7,11 +9,148 @@ class InformasiView extends GetView<InformasiController> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
-      body: Center(
-        child: Text(
-          'InformasiView is working',
-          style: TextStyle(fontSize: 20),
+      appBar: AppBar(
+        title: Text(
+          "Informasi",
+          style: FontConstant.heading2,
+        ),
+        centerTitle: true,
+      ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                'Selamat Datang di Halaman Informasi Pemilihan Umum!',
+                style: GoogleFonts.poppins(
+                  fontSize: 16,
+                  fontWeight: FontWeight.w700,
+                  color: Colors.black,
+                ),
+              ),
+              SizedBox(height: 8),
+              Text(
+                "Pemilihan umum adalah kesempatan bagi setiap warga negara untuk berpartisipasi aktif dalam menentukan arah masa depan bangsa. Suara Anda sangat berharga! Pemilihan umum ini memungkinkan setiap suara untuk didengar dan menentukan kepemimpinan serta kebijakan yang akan membawa perubahan bagi negara.",
+                textAlign: TextAlign.justify,
+                style: GoogleFonts.poppins(
+                  fontSize: 14,
+                  fontWeight: FontWeight.w500,
+                  color: Colors.black,
+                ),
+              ),
+              SizedBox(height: 16),
+              Text(
+                'Mengapa pemilihan umum penting?',
+                style: GoogleFonts.poppins(
+                  fontSize: 16,
+                  fontWeight: FontWeight.w700,
+                  color: Colors.black,
+                ),
+              ),
+              SizedBox(height: 8),
+              Text(
+                "Pemilihan umum adalah tulang punggung demokrasi. Melalui pemilihan, kita sebagai masyarakat memiliki kesempatan untuk memilih pemimpin yang terbaik, yang dapat mewakili aspirasi, kebutuhan, dan harapan kita. Partisipasi Anda dalam pemilihan ini:",
+                textAlign: TextAlign.justify,
+                style: GoogleFonts.poppins(
+                  fontSize: 14,
+                  fontWeight: FontWeight.w500,
+                  color: Colors.black,
+                ),
+              ),
+              SizedBox(height: 8),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: List.generate(
+                  controller.penting.length,
+                  (index) => Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 4.0),
+                    child: Row(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          '${index + 1}.', // Numbering
+                          style: TextStyle(
+                              fontSize: 16, fontWeight: FontWeight.bold),
+                        ),
+                        SizedBox(width: 8),
+                        Expanded(
+                          child: Text(
+                            controller.penting[index],
+                            textAlign: TextAlign.justify,
+                            style: TextStyle(fontSize: 16),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(height: 16),
+              Text(
+                'Siapa yang Berhak Memilih?',
+                style: GoogleFonts.poppins(
+                  fontSize: 16,
+                  fontWeight: FontWeight.w700,
+                  color: Colors.black,
+                ),
+              ),
+              SizedBox(height: 8),
+              Text(
+                "Anda berhak memilih dalam pemilihan umum ini jika:",
+                textAlign: TextAlign.justify,
+                style: GoogleFonts.poppins(
+                  fontSize: 14,
+                  fontWeight: FontWeight.w500,
+                  color: Colors.black,
+                ),
+              ),
+              SizedBox(height: 8),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: List.generate(
+                  controller.hak.length,
+                  (index) => Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 4.0),
+                    child: Row(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          '${index + 1}.', // Numbering
+                          style: TextStyle(
+                              fontSize: 16, fontWeight: FontWeight.bold),
+                        ),
+                        SizedBox(width: 8),
+                        Expanded(
+                          child: Text(
+                            controller.hak[index],
+                            textAlign: TextAlign.justify,
+                            style: TextStyle(fontSize: 16),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(height: 16),
+              ElevatedButton(
+                onPressed: () {
+                  Get.back();
+                },
+                child: Text(
+                  'Kembali',
+                  style: FontConstant.bodyStyle3,
+                ),
+                style: ButtonStyle(
+                    minimumSize: MaterialStateProperty.all<Size>(
+                  Size(double.infinity, 40),
+                )),
+              ),
+              SizedBox(height: 16),
+            ],
+          ),
         ),
       ),
     );
